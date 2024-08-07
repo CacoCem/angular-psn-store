488411e72dd802d02a747c630d30234eade797bd
Cauan Cem <cacocembritosoares@gmail.com>
2024-08-06 22:02:19 -0300

    Configuração do componente menu-bar

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,24 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
-  styleUrl: './menu-bar.component.css'
+  styleUrls: ['./menu-bar.component.css']
 })
-export class MenuBarComponent {
-  items: MenuItem[] = [];
+export class MenuBarComponent implements OnInit {
+  items: MenuItem[] = []; // Itens do menu
+  lastScrollTop = 0; // Última posição de rolagem
+  headerHeight = 50; // Altura do cabeçalho
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.items = [
-      { label: 'Jogos'},
-      { label: 'PS5'},
-      { label: 'PS4'},
-      { label: 'Serviços'},
-      { label: 'Acessórios'},
-      { label: 'Notícias'},
-      { label: 'Loja'},
-      { label: 'Suporte'}
+      { label: 'Jogos' },
+      { label: 'PS5' },
+      { label: 'PS4' },
+      { label: 'Serviços' },
+      { label: 'Acessórios' },
+      { label: 'Notícias' },
+      { label: 'Loja' },
+      { label: 'Suporte' }
     ];
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    const currentScrollTop = window.scrollY;
+    const header = document.querySelector('.header__menu-bar') as HTMLElement;
+    const tertiary = document.querySelector('.section__tertiary-container') as HTMLElement;
+
+    // Verifica se está rolando para baixo ou para cima
+    if (currentScrollTop > this.lastScrollTop) {
+      // Rolagem para baixo
+      tertiary?.classList.add('fixed');
+      header?.classList.remove('fixed');
+    } else {
+      // Rolagem para cima
+      tertiary?.classList.remove('fixed');
+      if (currentScrollTop > this.headerHeight) {
+        header?.classList.add('fixed');
+      }
+    }
   }
 }
